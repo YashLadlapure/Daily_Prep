36bd56a50c1a37c5f5855a092c3e00961d4dac46
Yash Ladlapure <yash.ladlapure@gmail.com>
2026-02-16 02:54:33 +0530

    Update STL1.cpp

--- STL1.cpp
+++ STL1.cpp
@@ -30,8 +30,53 @@ for(auto it:v)
     cout<<it<<" "<<"vector element using range based for loop"<<endl;
 }
 
+    //end is after the last element of vector
+v.erase(v.begin(),v.end());
+
+
+
+    //inserts 2 elements of value 4 at position 1
+v.insert(v.begin()+1,2,4);
+
+
+//removes the last element of vector
+v.pop_back();   
+
+
+// clears the vector
+v.clear();
+
+
+
+
+//Takes input from user and adds to vector
+int n;
+cout<<"Enter number of elements to add to vector: "<<endl;
+cin>>n;
+for(int i=0;i<n;i++)
+{
+    int x;
+    cin>>x;
+    v.push_back(x);
+}
+
+//prints full vector
+for(auto it:v)
+{
+    cout<<it<<" "<<"vector element after inserting 4 and popping last element"<<endl;
+}
+
+
+
+
+
+
+
+
+
 
 return 0;
+
 
 
 
