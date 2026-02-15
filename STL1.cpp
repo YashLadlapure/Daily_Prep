ceb416df7801a4621507e5dcffd7404c6cac0090
Yash Ladlapure <yash.ladlapure@gmail.com>
2026-02-15 18:22:33 +0530

    Update STL1.cpp

--- STL1.cpp
+++ STL1.cpp
@@ -1,1 +1,39 @@
-#incldue <bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    // pair
+pair<int,int> p{2,3};
+cout<<p.first<<" "<<"first element of pair"<<endl;
+cout<<p.second<<" "<<"second element of pair"<<endl;
+   
+
+
+    // vector
+vector<int> v;
+
+v.push_back(1);
+v.emplace_back(2);
+vector<int> v1{v};
+
+vector<int>::iterator it=v.begin();
+for(auto it=v.begin();it!=v.end();it++)
+{
+    cout<<*it<<" "<<"vector element"<<endl;
+
+}
+
+
+for(auto it:v)
+{
+    cout<<it<<" "<<"vector element using range based for loop"<<endl;
+}
+
+
+return 0;
+
+
+
+
+}
